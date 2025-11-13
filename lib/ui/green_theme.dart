fbb524262833a6933a45dd2434b19bdb6f4cdb7d
tammytamayao <tammytamayao@gmail.com>
2025-11-13 15:07:33 +0800

    feat: green chain color palette

---
+++ lib/ui/green_theme.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class GreenTheme {
+  static const primary = Color(0xFF5E8C61);
+  static const chip = Color(0xFF4F7652);
+  static const softBg = Color(0xFFF8FAF8);
+}
