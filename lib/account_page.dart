87427e209c3a66add65116dbc4939fd5b71e8a99
tammytamayao <tammytamayao@gmail.com>
2026-01-08 11:16:21 +0800

    feat: integrate fix in account page

--- lib/account_page.dart
+++ lib/account_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'auth_api.dart';
 import 'home_farmer.dart';
-import 'stalls_page.dart';
+import 'features/farmer/screens/stalls_page.dart';
 import 'login_page.dart';
 import 'driver_home.dart';
 import 'driver_drive_page.dart';
